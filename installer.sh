9ca8035b985998bfa5d1ff9001958b6e1d7d8161
al17091 <50813976+al17091@users.noreply.github.com>
2020-03-18 15:52:54 +0900

    Add files via upload

--- installer.sh
+++ installer.sh
@@ -26,6 +26,8 @@ git clone https://github.com/VundleVim/Vundle.vim.git "$INSTALL_DIR"
 
 
 echo 'copyed ~/.vimrc'
-git clone https://github.com/al17091/vimrc_git/vimrc_vundle ~/.vimrc
+git clone https://github.com/al17091/vimrc_git.git
+cp ./vimrc_git/vimrc_vundle ~/.vimrc
+rm -r ./vimrc_git
 vim +PluginInstall +qall
 
