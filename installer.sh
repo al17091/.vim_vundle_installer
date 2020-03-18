ffe6a91464c412916da63d44230d3b6054d83009
al17091 <50813976+al17091@users.noreply.github.com>
2020-03-18 15:43:15 +0900

    Add files via upload

---
+++ installer.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+#Installation directory
+BUNDLE_DIR=~/.vim/bundle
+INSTALL_DIR="$BUNDLE_DIR/vundle.vim"
+echo "install at $INSTALL_DIR"
+if [-e "$INSTALL_DIR"]; then
+    echo "$INSTALL_DIR already exists!"
+fi
+
+#check git command
+if type git; then
+    :#If you have git command, No problem.
+else
+    echo 'Please install git or update your path to include the git executable!'
+    exit 1
+fi
+
+#make bundle dir and fetch bundle
+echo "Begin fetching Vundle..."
+if ![-e "$INSTALL_DIR"]; then
+    mkdir -p "$BUNDLE_DIR"
+fi
+
+#If install directory already exist, install will contine.
+git clone https://github.com/VundleVim/Vundle.vim.git "$INSTALL_DIR"
+
+
+echo 'copyed ~/.vimrc'
+git clone https://github.com/al17091/vimrc_vundle ~/.vimrc
+vim +PluginInstall +qall
+
